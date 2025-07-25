5b131725f2bd3557259528b1a74e1549b13a615c
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-25 05:56:44 +0300

    updated

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -5,82 +5,81 @@ import MuxPlayer from "@mux/mux-player-react";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import { Loader2,Lock } from "lucide-react";
+import { Loader2, Lock } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useConfettiStore } from "@/hooks/use-confetti-store";
 
-interface VideoPlayerProps{
-    playerId:string;
-courseId:string;
-chapterId:string;
-nextChapterId?:string;
-isLocked:boolean;
-completeInEnd:boolean;
-title:string;
+interface VideoPlayerProps {
+  playbackId: string;
+
+  courseId: string;
+  chapterId: string;
+  nextChapterId?: string;
+  isLocked: boolean;
+  completedOnEnd: boolean;
+  title: string;
 }
 
-export const VideoPlayer=({playbackId,courseId,chapterId,nextChapterId,isLocked,completedOnEnd,title}:VideoPlayerProps)=>{
+export const VideoPlayer = ({
+  playbackId,
+  courseId,
+  chapterId,
+  nextChapterId,
+  isLocked,
+  completedOnEnd,
+  title,
+}: VideoPlayerProps) => {
+  const [isReady, setIsReady] = useState(false);
+  const router = useRouter();
+  const confetti = useConfettiStore();
+  const onEnd = async () => {
+    try {
+      if (completedOnEnd) {
+        await axios.put(
+          `/api/courses/${courseId}/chapters/${chapterId}/progress`,
+          {
+            isCompleted: true,
+          }
+        );
+        if (!nextChapterId) {
+          confetti.onOpen();
+        }
 
-    const [isReady,setIsReady]=useState(false);
-    const router=useRouter();
-    const confetti=useConfettiStore();
-    const onEnd=async()=>{
-        try{
-            if(completedOnEnd){
-                await axios.put(`/api/courses/${courseId}/chapters/${chapterId}/progress`,{
-                    isCompleted:true,
-                });
-if(!nextChapterId){
-    confetti.onOpen();
-}
+        toast.success("Progress Updated");
+        router.refresh();
 
-toast.success("Progress Updated");
-router.refresh();
-
-if(nextChapterId){
-    router.push(`/courses/${courseId}/chapters/${nextChapterId}`)
-}
-            }
-
-
-        }catch{
-            toast.error("Something went wrong");
-        } 
+        if (nextChapterId) {
+          router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
+        }
+      }
+    } catch {
+      toast.error("Something went wrong");
     }
-return(
+  };
+  return (
     <div className="relative aspect-video ">
-{!isReady &&  isLocked &&(
-    <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
-        <Loader2 className="h-8 w-8 animate-spin text-secondary"/>
+      {!isReady && isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="h-8 w-8 animate-spin text-secondary" />
+        </div>
+      )}
+      {isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+          <Lock className="h-8 w-8" />
+          <p className="text-sm">This chapter is locked</p>
+        </div>
+      )}
+      ;
+      {!isLocked && (
+        <MuxPlayer
+          title={title}
+          className={cn(!isReady && "hidden")}
+          onCanPlay={() => setIsReady(true)}
+          onEnded={onEnd}
+          autoPlay
+          playbackId={playbackId}
+        />
+      )}
     </div>
-)}
-{isLocked &&(
-    <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
-        <Lock className="h-8 w-8"/>
-        <p className="text-sm">
-            This chapter is locked
-        </p>
-
-    </div>
-        )
-        };
-
-
-{!isLocked&&(
-    <MuxPlayer
-    title={title}
-    className={cn(!isReady && "hidden")}
-    onCanPlay={()=>setIsReady(true)}
-    onEnded={onEnd}
-    autoPlay
-    playbackId={playbackId}
-    
-    />
-)}
-
-
-
-    </div>
-)
-
-}
+  );
+};
